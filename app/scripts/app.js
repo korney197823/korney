036c8b01a73079da01a6a1ea337ea24200cc2963
Denis Kornienko <korney197823@gmail.com>
2017-07-28 21:30:28 +0300

    add pseudo elements in title, toggle authorize button

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,12 @@ import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+
+	// Переворот карточки по клику на кнопку
+	const loginButton = document.querySelector('.btn_auto');
+	const flipper = document.querySelector('.flipper');
+
+	loginButton.addEventListener('click', () => {
+		flipper.classList.toggle('flip');
+	});
 });
